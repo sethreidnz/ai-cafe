5332ab654403dc1e8b90dc735ae77a615fe3a52b
Seth Reid <contact@sethreid.co.nz>
2018-04-03 23:43:28 +1200

    add chat

--- src/client/src/bot/index.js
+++ src/client/src/bot/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Chat } from "botframework-webchat";
 import "./Bot.css";
 
 export class Bot extends Component {
@@ -7,6 +8,10 @@ export class Bot extends Component {
       <div className="orders container">
         <div className="columns">
           <div class="column">
+            <Chat
+              directLine={{ secret: direct_line_secret }}
+              user={{ id: "1", name: "seth" }}
+            />
           </div>
         </div>
       </div>
