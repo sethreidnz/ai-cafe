180b5e80244fa863dfc2a4c7216c2db9d128ad99
Seth Reid <contact@sethreid.co.nz>
2018-04-03 19:18:10 +1200

    deleted build

--- src/server/build/database/contants.js
+++
@@ -1,6 +0,0 @@
-export const DATABASE_NAME = "AICafe";
-export const BOT_DATA_COLLECTION_NAME = "botdata";
-export const ORDERS_COLLECTION_NAME = "orders";
-export const RESERVATIONS_COLLECTION_NAME = "reservations";
-export const REVIEWS_COLLECTION_NAME = "reviews";
-//# sourceMappingURL=contants.js.map
--- src/server/build/routes/messages.js
+++
@@ -1,6 +0,0 @@
-import { connector } from '../bot';
-
-export default (router => {
-  router.post("/api/messages", connector.listen());
-});
-//# sourceMappingURL=messages.js.map
